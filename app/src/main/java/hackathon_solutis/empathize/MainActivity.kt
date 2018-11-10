9b2e046b3e09cd94b12d265c76387eb48a1c5e98
João Gabriel Santana Sousa <joao.sousa@solutis.com.br>
2018-11-10 15:22:58 -0300

    first commit

---
+++ app/src/main/java/hackathon_solutis/empathize/MainActivity.kt
@@ -0,0 +1,40 @@
+package hackathon_solutis.empathize
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        val salvador = LatLng(-12.981312, -38.456414)
+
+        val markers = listOf(MarkerOptions().position(sydney).title("Marker in Sydney"), MarkerOptions().position(sydney).title("Marker in Sydney"))
+
+        markers.forEach {
+            mMap.addMarker(it)
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(salvador))
+    }
+}
